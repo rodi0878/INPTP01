1c9e7249038bc05c9901f6ba2328b6d62246548d
Martin Fryml <st46557@student.upce.cz>
2017-10-10 20:06:22 +0200

    Added more boss tests

--- INPTP_AppForFixingTests/Entity/BossTests.cs
+++ INPTP_AppForFixingTests/Entity/BossTests.cs
@@ -18,7 +18,7 @@ namespace INPTP_AppForFixing.Tests
             {
                 MonthlySalaryCZK = 1000
             };
-            boss.InsertEmpl(new Employee());
+            boss.InsertEmployee(new Employee());
 
             Assert.AreEqual(12000, boss.CalcYearlySalaryCZK());
         }
@@ -31,7 +31,7 @@ namespace INPTP_AppForFixing.Tests
                 MonthlySalaryCZK = 1000
             };
             boss.SetSalaryBonus(100);
-            boss.InsertEmpl(new Employee());
+            boss.InsertEmployee(new Employee());
 
             Assert.AreEqual(12000 + 1200, boss.CalcYearlySalaryCZK());
         }
@@ -42,7 +42,7 @@ namespace INPTP_AppForFixing.Tests
         {
             Employee empl;
             Boss boss = new Boss(new Department());
-            boss.InsertEmpl(empl = new Employee());
+            boss.InsertEmployee(empl = new Employee());
 
             Assert.IsTrue(boss.HasEmployee(empl));
         }
@@ -51,9 +51,9 @@ namespace INPTP_AppForFixing.Tests
         public void BossIsAbleToCountSubemployeesTest()
         {
             Boss boss = new Boss(new Department());
-            boss.InsertEmpl(new Employee());
-            boss.InsertEmpl(new Employee());
-            boss.InsertEmpl(new Employee());
+            boss.InsertEmployee(new Employee());
+            boss.InsertEmployee(new Employee());
+            boss.InsertEmployee(new Employee());
 
             Assert.AreEqual(3, boss.EmployeeCount);
         }
@@ -72,11 +72,11 @@ namespace INPTP_AppForFixing.Tests
             Boss boss = new Boss(new Department());
             Employee empl = new Employee();
 
-            boss.InsertEmpl(empl);
+            boss.InsertEmployee(empl);
             Assert.AreEqual(1, boss.EmployeeCount);
             Assert.IsTrue(boss.HasEmployee(empl));
 
-            boss.PurgeEmpl(empl);
+            boss.PurgeEmployee(empl);
             Assert.AreEqual(0, boss.EmployeeCount);
         }
 
@@ -88,6 +88,47 @@ namespace INPTP_AppForFixing.Tests
 
             Assert.AreEqual(0, boss.EmployeeCount);
             Assert.IsFalse(boss.HasEmployee(empl));
+        }
+
+        [Test()]
+        public void BossGetsAllEmployees()
+        {
+            Boss boss = new Boss(new Department());
+            HashSet<Employee> employees = new HashSet<Employee>();
+            Employee empl1 = new Employee();
+            Employee empl2 = new Employee();
+            Employee empl3 = new Employee();
+
+            boss.InsertEmployee(empl1);
+            boss.InsertEmployee(empl2);
+            boss.InsertEmployee(empl3);
+
+            employees.Add(empl1);
+            employees.Add(empl2);
+            employees.Add(empl3);
+
+            Assert.AreEqual(employees, boss.GetEmployees());
+        }
+
+        [Test()]
+        public void BossGetsAllEmployeesAfterKickingEmployee()
+        {
+            Boss boss = new Boss(new Department());
+            HashSet<Employee> employees = new HashSet<Employee>();
+            Employee empl1 = new Employee();
+            Employee empl2 = new Employee();
+            Employee empl3 = new Employee();
+
+            boss.InsertEmployee(empl1);
+            boss.InsertEmployee(empl2);
+            boss.InsertEmployee(empl3);
+
+            boss.PurgeEmployee(empl2);
+
+            employees.Add(empl1);
+            employees.Add(empl3);
+
+            Assert.AreEqual(employees, boss.GetEmployees());
         }
     }
 }
