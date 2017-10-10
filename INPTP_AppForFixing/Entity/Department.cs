02f85eb9563a601359c98207362fa8fa75c612c9
D V <st43357@student.upce.cz>
2017-10-10 14:34:28 +0200

    Treti commit

--- INPTP_AppForFixing/Entity/Department.cs
+++ INPTP_AppForFixing/Entity/Department.cs
@@ -2,25 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace INPTP_AppForFixing
 {
     public class Department
     {
+        /// <summary>
+        /// Property for get and set name of departement
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Property Code with private setter and getter which return code composed of 
+        /// prefix "DPT-" and Name which has modified some chars using regular expressions
+        /// </summary>
         public string Code
         {
             get
             {
-                string dptCode = "DPT-";
-                dptCode += Name.Replace(" ", "-").Replace(":","-").Replace(",","-").Replace(".","-").Replace("--","-").ToUpper();
-
-                return dptCode;
+                string dptCode = "DPT-" + Regex.Replace(Name, "[ :,.]", "-").ToUpper();
+                return Regex.Replace(dptCode, "[-]{2}", "-").ToUpper();
             }
         }
 
+        /// <summary>
+        /// Method ToString converts an object to its string representation.
+        /// </summary>
+        /// <returns>Code of department</returns>
         public override string ToString()
         {
             return Code;
