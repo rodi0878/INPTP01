f4c4e1ad3b6392d24f5a331d42a4af1e0a26ffe1
seri300 <sereda@student.vspj.cz>
2017-10-26 12:00:48 +0200

    Revert "Edit EmploieeClass"

--- INPTP_AppForFixingTests/Entity/BossTests.cs
+++ INPTP_AppForFixingTests/Entity/BossTests.cs
@@ -16,11 +16,11 @@ namespace INPTP_AppForFixing.Tests
         {
             Boss boss = new Boss(new Department())
             {
-                MonthlySalaryCZK = 1000
+                salary = 1000
             };
-            boss.InsertEmpl(new Employee());
+            boss.InsertEmpl(new EmployeeClass());
 
-            Assert.AreEqual(12000, boss.CalcYearlySalaryCZK());
+            Assert.AreEqual(12000, boss.YearlySalary());
         }
 
         [Test()]
@@ -28,35 +28,21 @@ namespace INPTP_AppForFixing.Tests
         {
             Boss boss = new Boss(new Department())
             {
-                MonthlySalaryCZK = 1000
+                salary = 1000
             };
-            boss.PerEmplSalaryBonus = 100;
-            boss.InsertEmpl(new Employee());
+            boss.SetSalaryBonus(100);
+            boss.InsertEmpl(new EmployeeClass());
 
-            Assert.AreEqual(12000 + 1200, boss.CalcYearlySalaryCZK());
+            Assert.AreEqual(12000 + 1200, boss.YearlySalary());
         }
 
-        [Test()]
-        public void BossWithTwoSubEmplYearlyIncomeBonusTest()
-        {
-            Boss boss = new Boss(new Department())
-            {
-                MonthlySalaryCZK = 1000
-            };
-            boss.PerEmplSalaryBonus = 100;
-            boss.InsertEmpl(new Employee());
-            boss.InsertEmpl(new Employee());
-
-            Assert.AreEqual((12000 * (1 - Boss.TaxRate)) + (2 * 1200 * (1 - Boss.TaxRate)),
-                boss.CalcYearlyIncome());
-        }
 
         [Test()]
         public void BossCanHaveSubemployeesTest()
         {
-            Employee empl;
+            EmployeeClass empl;
             Boss boss = new Boss(new Department());
-            boss.InsertEmpl(empl = new Employee());
+            boss.InsertEmpl(empl = new EmployeeClass());
 
             Assert.IsTrue(boss.HasEmployee(empl));
         }
@@ -65,9 +51,9 @@ namespace INPTP_AppForFixing.Tests
         public void BossIsAbleToCountSubemployeesTest()
         {
             Boss boss = new Boss(new Department());
-            boss.InsertEmpl(new Employee());
-            boss.InsertEmpl(new Employee());
-            boss.InsertEmpl(new Employee());
+            boss.InsertEmpl(new EmployeeClass());
+            boss.InsertEmpl(new EmployeeClass());
+            boss.InsertEmpl(new EmployeeClass());
 
             Assert.AreEqual(3, boss.EmployeeCount);
         }
@@ -84,7 +70,7 @@ namespace INPTP_AppForFixing.Tests
         public void BossIsAbleToKickSubemployeeTest()
         {
             Boss boss = new Boss(new Department());
-            Employee empl = new Employee();
+            EmployeeClass empl = new EmployeeClass();
 
             boss.InsertEmpl(empl);
             Assert.AreEqual(1, boss.EmployeeCount);
@@ -98,51 +84,10 @@ namespace INPTP_AppForFixing.Tests
         public void BossIsAbleToIgnoreForeignEmployeesTest()
         {
             Boss boss = new Boss(new Department());
-            Employee empl = new Employee();
+            EmployeeClass empl = new EmployeeClass();
 
             Assert.AreEqual(0, boss.EmployeeCount);
             Assert.IsFalse(boss.HasEmployee(empl));
-        }
-
-        [Test()]
-        public void BossGetsAllEmployees()
-        {
-            Boss boss = new Boss(new Department());
-            HashSet<Employee> employees = new HashSet<Employee>();
-            Employee empl1 = new Employee();
-            Employee empl2 = new Employee();
-            Employee empl3 = new Employee();
-
-            boss.InsertEmpl(empl1);
-            boss.InsertEmpl(empl2);
-            boss.InsertEmpl(empl3);
-
-            employees.Add(empl1);
-            employees.Add(empl2);
-            employees.Add(empl3);
-
-            Assert.AreEqual(employees, boss.GetEmployees());
-        }
-
-        [Test()]
-        public void BossGetsAllEmployeesAfterKickingEmployee()
-        {
-            Boss boss = new Boss(new Department());
-            HashSet<Employee> employees = new HashSet<Employee>();
-            Employee empl1 = new Employee();
-            Employee empl2 = new Employee();
-            Employee empl3 = new Employee();
-
-            boss.InsertEmpl(empl1);
-            boss.InsertEmpl(empl2);
-            boss.InsertEmpl(empl3);
-
-            boss.PurgeEmpl(empl2);
-
-            employees.Add(empl1);
-            employees.Add(empl3);
-
-            Assert.AreEqual(employees, boss.GetEmployees());
         }
     }
 }
